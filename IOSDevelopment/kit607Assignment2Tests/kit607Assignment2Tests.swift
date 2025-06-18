88d779ac00b56ba8206e2b628178d05985b2d6b5
CCChen <50975978+MykiChen@users.noreply.github.com>
2025-06-18 16:55:34 +1000

    Add files via upload

---
+++ IOSDevelopment/kit607Assignment2Tests/kit607Assignment2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  kit607Assignment2Tests.swift
+//  kit607Assignment2Tests
+//
+//  Created by Myki on 2020/4/21.
+//  Copyright © 2020 University of Tasmania. All rights reserved.
+//
+
+import XCTest
+@testable import kit607Assignment2
+
+class kit607Assignment2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
